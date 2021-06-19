167fb27d85be3867bf5e9f9456fbc0408313235c
TJ Littlejohn <teej@TJs-MacBook-Pro.local>
2021-06-19 15:06:16 -0400

    first

---
+++ SharePlayCombineApp/SharePlayCombineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharePlayCombineAppApp.swift
+//  SharePlayCombineApp
+//
+//  Created by TJ Littlejohn on 6/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SharePlayCombineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
